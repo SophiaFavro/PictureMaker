fc206865a1b334eae9d48a527e1c5aa23c7bba23
SophiaFavro <72363654+SophiaFavro@users.noreply.github.com>
2023-03-08 21:30:24 -0800

    Added color picker

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+var colorPicker;
+
 function setup() {
     let canvas = createCanvas(windowWidth, windowHeight);
-    //canvas.position(windowWidth/2 - (windowWidth/4), 0);
+    colorPicker = createColorPicker("blue");
+    colorPicker.position(25, 350);
     background(204);
     fill("black");
+    text("Choose your color", 25, 400);
     let circle = ellipse(50, 40, 50);
     text("press c for circle", 25, 80);
     let square = rect(25, 100, 50, 50);
     text("press s for square", 25, 175);
     let rectangle = rect(25, 200, 50, 100);
     text("press r for rectangle", 25, 325);
+    
   }
 
   function draw() {
