df63c3b96f6d5521f90e9785353127049ef6eb31
SophiaFavro <72363654+SophiaFavro@users.noreply.github.com>
2023-03-08 20:22:44 -0800

    added shape picker

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ function setup() {
     //canvas.position(windowWidth/2 - (windowWidth/4), 0);
     background(204);
     fill("black");
-    ellipse(50, 40, 50);
-    rect(50, 100, 33, 33);
+    let circle = ellipse(50, 40, 50);
+    text("press c for circle", 25, 80);
+    let square = rect(25, 100, 50, 50);
+    text("press s for square", 25, 175);
+    let rectangle = rect(25, 200, 50, 100);
+    text("press r for rectangle", 25, 325);
   }
 
   function draw() {
   }
 
   function mousePressed() {
+
   }
