717df0075c4316fba112b4b1fe3b99a53f21dcef
SophiaFavro <72363654+SophiaFavro@users.noreply.github.com>
2023-03-08 19:52:02 -0800

    set up canvas, some of the web tool chain

---
+++ index.js
@@ -0,0 +1,14 @@
+function setup() {
+    let canvas = createCanvas(windowWidth, windowHeight);
+    //canvas.position(windowWidth/2 - (windowWidth/4), 0);
+    background(204);
+    fill("black");
+    ellipse(50, 40, 50);
+    rect(50, 100, 33, 33);
+  }
+
+  function draw() {
+  }
+
+  function mousePressed() {
+  }
